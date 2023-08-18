e3008e7013ad4cee59d0144cda46900855f2f7de
H1R0Z1N <joaoo99966@gmail.com>
2023-08-18 17:06:16 -0300

    bot adicionado ao github

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # EmailBot
 > Bot envio de emails em python, normalmente ultilizado em empresas, sites e apps para o envio em massa de e-mails. registrados.
+
+## Requisitos
+1. Criei ultilizando o app PyDroid3 no mobile.
+1. Versão Atual
+     - Python 3.9.7
+1. Moldolos usados no projeto
+     - smtplib
+          - pandas
+               - time
+## Modo de uso
+Para definir para que email vai ser enviado modifique o arquivo `/root_bot/users.xlsx`
+![img dos users](img/users.jpg)
+Após isso escreva sua menssagem no arquivo `/root_bot/menssagem.html` e execute o `main.py' para fazer os envios
+### ScreenShot
+![Imagem do ps1](img/app.jpg)
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+#!/usr/bin/python3
+import pandas as pd
+import smtplib
+import email.message
+import time, os
 
-print("novo bot")
+class bot_email(object):
+    def __init__(self):
+        self.env_msg()
+    def env_msg(self):
+        arq = open ('root_bot/menssagem.html','r')
+        corpo_email = arq.read()
+        arq.close()
+        clientes = pd.read_excel('./root_bot/users.xlsx')
+        for index, ct in clientes.iterrows():
+            msg = email.message.Message()
+            msg['Subject'] = 'Teste de mensagens automáticas'
+            msg['From'] = 'lucasfodzap@gmail.com'
+            msg['To'] = ct['email']
+            password = 'goxngdokqxjodaxx'
+            msg.add_header('Content-Type', 'text/html')
+            msg.set_payload(corpo_email)
+            server = smtplib.SMTP('smtp.gmail.com: 587')
+            server.starttls()
+            server.login(msg['From'], password)
+            server.sendmail(msg['From'],msg['To'], msg.as_string().encode('utf-8'))
+            print("Enviado com sucesso!\nPara:",ct['nome'],'\n')
+            server.quit()
+            time.sleep(5)
+bot_email()
 
